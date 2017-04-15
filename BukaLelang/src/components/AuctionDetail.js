ba2c3b10f5f694e22abd04cd127e737f2dcbf6f0
mrhandoko <bambank.handoko@yahoo.com>
2017-04-15 17:39:22 +0700

    header

--- BukaLelang/src/components/AuctionDetail.js
+++ BukaLelang/src/components/AuctionDetail.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 import { Actions } from 'react-native-router-flux'
-import { Image } from 'react-native'
-import { Container, Content, Button, Text, Footer, FooterTab, Icon } from 'native-base'
+import { Image, TouchableOpacity } from 'react-native'
+import { Container, Content, Button, Text, Footer, FooterTab, Icon, Header, Left, Body, Title } from 'native-base'
 
 import HeaderNav from './HeaderNav'
 import Styles from '../assets/styles/Home.styles'
 
 class AuctionDetail extends Component {
+  constructor () {
+    super()
+    this.state = {
+      bidPrice: 0
+    }
+  }
+
   render () {
     return (
       <Container>
-        <HeaderNav />
-        <Image source={{ uri: 'http://4.bp.blogspot.com/-IgOZa3Gm5Qc/VecNGI-7NJI/AAAAAAAAAk0/0bg1FVdAfes/s1600/Foto-Isyana-Sarasvati-01.jpg' }} style={{width: '100%', height: '71%' }} />
+      <Header style={Styles.Header}>
+        <Left>
+          <Button transparent style={Styles.BurgerMenu}>
+          <Icon name='menu' onPress={() => Actions.refresh({key: 'MenuDrawer', open: value => !value})} />
+          </Button>
+        </Left>
+        <Body>
+          <Title style={Styles.Logo}>BukaLelang</Title>
+        </Body>
+        <Body>
+          <TouchableOpacity style={Styles.TouchSearch} onPress={Actions.Search }>
+            <Icon name='ios-search' style={Styles.Search}/>
+          </TouchableOpacity>
+        </Body>
+        <Image source={{ uri: 'http://4.bp.blogspot.com/-IgOZa3Gm5Qc/VecNGI-7NJI/AAAAAAAAAk0/0bg1FVdAfes/s1600/Foto-Isyana-Sarasvati-01.jpg' }} style={{width: '100%', height: '71%' }}>
+        </Image>
         <Footer style={{ height: 40}}>
           <FooterTab style={{ backgroundColor: '#E29A09' }}>
-            <Button>
-              <Text style={Styles.FooterText}>Rp. 0,-</Text>
-            </Button>
+
           </FooterTab>
         </Footer>
         <Footer>
