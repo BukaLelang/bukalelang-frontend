b98401eb9c48ddcc34dc5d2a860b68c66a6f52fc
pisanggoreng <7ofpentacles@gmail.com>
2017-04-16 16:38:35 +0700

    bla bla bla

--- BukaLelang/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Wrapper from './components/Wrapper'
-
-const App = () => (
-  <Wrapper />
-)
-
-export default App
--- BukaLelang/src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
-
-import rootReducers from '../reducers'
-
-const store = createStore(rootReducers, applyMiddleware(thunk, logger))
-
-export default store
