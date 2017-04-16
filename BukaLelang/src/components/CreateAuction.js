e7a02bdeb50755b8febddaf10a78c9778ae921e5
mrhandoko <bambank.handoko@yahoo.com>
2017-04-16 17:18:52 +0700

    adding create auction

--- BukaLelang/src/components/CreateAuction.js
+++ BukaLelang/src/components/CreateAuction.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import { Container, Content, InputGroup, Input } from 'native-base'
+
+import HeaderNav from './HeaderNav'
 
 class CreateAuction extends Component {
-  render() {
+  componentWillMount () {
+    Actions.refresh({key: 'MenuDrawer', open: value => !value})
+  }
+
+  render () {
     return (
-      <Text>This is CreateAuction</Text>
+      <Container>
+        <HeaderNav />
+        <Content>
+          <InputGroup regular>
+            <Input placeholder='Nama Barang' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Deskripsi Barang' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Berat (kg)' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Min. Price' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Max. Price' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Kelipatan Bidding' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Tanggal Mulai Lelang' />
+          </InputGroup>
+          <InputGroup regular>
+            <Input placeholder='Tanggal Akhir Lelang' />
+          </InputGroup>
+        </Content>
+      </Container>
     )
   }
 }
