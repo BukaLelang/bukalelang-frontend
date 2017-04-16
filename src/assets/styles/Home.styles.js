9feb5f2269eaed6b50e82e83b37d2c9ac28a5f27
pisanggoreng <7ofpentacles@gmail.com>
2017-04-16 16:10:20 +0700

    selesai fitur login,register,logout

---
+++ src/assets/styles/Home.styles.js
@@ -0,0 +1,33 @@
+export default {
+  Header: {
+    backgroundColor:'#a30f42'
+  },
+  Footer: {
+    backgroundColor: '#a30f42'
+  },
+  ParticipateButton: {
+    backgroundColor: '#E29A09',
+    margin: 10
+  },
+  FooterText: {
+    color: '#ffffff'
+  },
+  TextMenu: {
+    color: '#F75D74'
+  },
+  LogoutButton: {
+    backgroundColor: '#f7f7f7',
+    margin: 10
+  },
+  LogoutText: {
+    color: '#CCCCCC'
+  },
+  Logo: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'Iowan Old Style',
+    marginTop: 20,
+    marginBottom: 20
+  }
+}
---
+++ src/components/Wrapper.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Scene, Router } from 'react-native-router-flux'
+
+import Home from './Home'
+import Login from './Login'
+import Register from './Register'
+import MenuDrawer from './MenuDrawer'
+import Search from './Search'
+
+const Wrapper = () => (
+  <Router>
+    <Scene key='root'>
+      <Scene key='MenuDrawer' component={MenuDrawer} open={false}>
+        <Scene key='Home' component={Home} hideNavBar />
+      </Scene>
+      <Scene key='Login' component={Login} initial={true} hideNavBar />
+      <Scene key='Register' component={Register} hideNavBar />
+      <Scene key='Search' component={Search} hideNavBar />
+    </Scene>
+  </Router>
+)
+
+export default Wrapper
